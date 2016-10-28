a7171a01de9ee2585d6ec8594fa123f90960d7ee
Ruslan Durov <s.t.o.k.a.t.p@gmail.com>
2016-10-28 17:09:50 +0300

    TFS Complete

--- Assets/Scripts/GameEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public static class GameEvent {
-    public const string ENEMY_HIT = "ENEMY_HIT";
-    public const string SPEED_CHANGED = "SPEED_CHANGED";
-}
