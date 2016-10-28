461e2fcf641bea51bb589f85c13fc97dc96eb0f4
Ruslan Durov <s.t.o.k.a.t.p@gmail.com>
2016-10-28 12:05:12 +0300

    Start (import FPS)

---
+++ Assets/Scripts/SettingsPopUp.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SettingsPopUp : MonoBehaviour {
+    [SerializeField]
+    private Slider speedSlider;
+
+    void Start()
+    {
+        speedSlider.value = PlayerPrefs.GetFloat("speed", 1);
+    }
+
+    public void Open()
+    {
+        gameObject.SetActive(true); // Открываем окно
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false); // Прячем
+    }
+
+    public void OnSubmitName(string name)
+    {
+        Debug.Log(name);
+    }
+
+    public void OnSpeedValue(float speed)
+    {
+        Messenger<float>.Broadcast(GameEvent.SPEED_CHANGED, speed);
+    }
+}
